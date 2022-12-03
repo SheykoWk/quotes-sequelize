367084f67a73f460ba5c2baccb8e487380deab15
Sh3yk0  (Sahid Kick) <sheyko.wk@gmail.com>
2022-12-02 21:54:40 -0600

    add protected routes

--- src/users/users.router.js
+++ src/users/users.router.js
@@ -1,11 +1,12 @@
 const router = require('express').Router()
+const JwtPassport = require('../middleware/auth.middleware')
 
 const userServices = require('./users.services')
 
-router.get("/", userServices.getAllUsers) //? /api/v1/users
+router.get("/", JwtPassport.authenticate('jwt', {session: false}) , userServices.getAllUsers) //? /api/v1/users
 router.post("/", userServices.postUser) //? /api/v1/users
 
-router.get("/:id", userServices.getUserById) //? /api/v1/users/:id
+router.get("/:id", JwtPassport.authenticate('jwt', {session: false}) , userServices.getUserById) //? /api/v1/users/:id
 router.patch('/:id', userServices.patchUser) //? /api/v1/users/:id
 router.delete('/:id', userServices.deleteUser) //? /api/v1/users/:id
 
